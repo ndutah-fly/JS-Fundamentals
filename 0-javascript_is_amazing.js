af9cba7e73fe247225ee92f7018595bbf3659cef
Effie Nduta <ndutaeffie22@gmail.com>
2025-11-11 10:06:01 +0300

    Add script that prints 'Javascript is amazing'

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,4 +1,2 @@
-0-javascript_is_amazing.javascript_is_amazing
-const myVar = "Javascript is amazing" ;
-console.log(myVar)
-node 0-javascript_is_amazing.javascript_is_amazing
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
